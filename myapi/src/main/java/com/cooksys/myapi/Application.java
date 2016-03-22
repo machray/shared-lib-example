33ab188a06656f95b45e1121e6f40d7f0f14952b
Tim Davidson <tndavidson@gmail.com>
2016-03-22 07:15:22 -0600

    initial commit

---
+++ myapi/src/main/java/com/cooksys/myapi/Application.java
@@ -0,0 +1,11 @@
+
+package com.cooksys.myapi;
+
+import com.cooksys.sharedlib.MySharedLibClass;
+
+public class Application {
+	public static void main(String[] args) {
+		MySharedLibClass lib = new MySharedLibClass();
+		lib.doStuff();
+	}
+}
---
+++ sharedlib/src/main/java/com/cooksys/sharedlib/MySharedLibClass.java
@@ -0,0 +1,7 @@
+package com.cooksys.sharedlib;
+
+public class MySharedLibClass {
+	public void doStuff() {
+		System.out.println("Doing stuff");
+	}
+}
